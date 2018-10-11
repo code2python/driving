65c686c9123f330222494468ab78d71120ed5c10
Jerry Tseng <0817jerry@gmail.com>
2018-10-11 11:25:14 +1100

    add USA

--- driving.py
+++ driving.py
@@ -1,8 +1,15 @@
+# Nationality must start with capital letter 
+
 country = input ('What is your nationality: ')
 age = input ('What is your age: ')
 age = int (age)
 if country == 'Taiwan':
 	if age >= 18:
+		print('you can take the drivers license test')
+	else:
+		print('you are not yet able to take your drivers license test')
+elif country == 'USA':
+	if age >= 16:
 		print('you can take the drivers license test')
 	else:
 		print('you are not yet able to take your drivers license test')
