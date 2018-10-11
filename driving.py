a3fdd1ab47aae3e735f60f7ab3dfbf460bb6ae7d
Jerry Tseng <0817jerry@gmail.com>
2018-10-11 11:15:59 +1100

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,9 @@
+country = input ('What is your nationality: ')
+age = input ('What is your age: ')
+age = int (age)
+if country == 'Taiwan':
+	if age >= 18:
+		print('you can take the drivers license test')
+	else:
+		print('you are not yet able to take your drivers license test')
+		
